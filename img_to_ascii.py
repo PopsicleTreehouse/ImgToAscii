535b26d0c54548c9ec63bef3e6da71e960b310de
PopsicleTreehouse <ahoynodnarb@gmail.com>
2022-11-03 13:20:44 -0700

    Update img_to_ascii.py

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -5,6 +5,17 @@ from PIL import Image
 import numpy as np
 
 
+def get_sorted_weights():
+    with open("ordered_chars.txt", "r") as f:
+        lines = []
+        for line in f.readlines():
+            char_code = int(line)
+            ch = chr(char_code)
+            lines.append(ch)
+
+    return lines
+
+
 def get_pixels(img_path, resize=1.0):
     with open(img_path, "rb") as fin:
         img = Image.open(fin).convert("RGBA")
@@ -14,19 +25,29 @@ def get_pixels(img_path, resize=1.0):
 
 
 def get_as_ascii(img_path, scale):
-    DENSITY = " .'`^\",:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@"
+    DENSITY = get_sorted_weights()
     DENSITY_LEN = len(DENSITY)
     lines = []
     pixels = get_pixels(img_path, resize=scale)
     for row in pixels:
-        ret = ""
+        line = []
         for pixel in row:
             r, g, b, a = pixel.astype(np.uint16) / 255
             brightness = (r + g + b) / 3 * a
             idx = round(brightness * (DENSITY_LEN - 1))
-            ret += "%-2s" % (DENSITY[idx])
-        lines.append(ret)
-    return "\n".join(lines)
+            ch = DENSITY[idx]
+            line.append(ch)
+        lines.append(line)
+
+    ret = []
+    for line in lines:
+        l = ""
+        for ch in line:
+            s = ch
+            l += s + "\t".expandtabs(1)
+        ret.append(l)
+
+    return "\n".join(ret)
 
 
 if __name__ == "__main__":
@@ -35,23 +56,24 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--path",
-        nargs=1,
         type=str,
         required=True,
         help="path to input image",
         dest="path",
+        nargs="?",
     )
     parser.add_argument(
         "--scale",
-        nargs=1,
         type=float,
         default=1.0,
         required=False,
         help="rescale factor of the output resolution",
         dest="scale",
+        nargs="?",
     )
     args = vars(parser.parse_args())
-    img_path = args["path"][0]
-    scale = args["scale"][0]
+    print(args)
+    img_path = args["path"]
+    scale = args["scale"]
     art = get_as_ascii(img_path, scale=scale)
     print(art)
